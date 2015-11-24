31a9aa7fb0dfa2d980edf5157c7aa8dc0f771a02
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-24 06:40:50 -0500

    Change to fix Heroku Postgres integration

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -21,8 +21,12 @@ environments {
         dataSource {
             dbCreate = "update"
             driverClassName = "org.postgresql.Driver"
+            println "DATABASE_URL=${System.env.DATABASE_URL}"
             uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
             url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+            println "Host=$uri.host"
+            println "Port=$uri.port"
+            println "Host=$uri.path"
             username = uri.userInfo.split(":")[0]
             password = uri.userInfo.split(":")[1]
         }
